25ca0ad2322fe3a60db8a1610d4476a3507b9f21
diligejy <sjy049@gmail.com>
2020-09-30 14:37:35 +0900

    flask api step1

--- flask_fb/project2/app.py
+++ flask_fb/project2/app.py
@@ -1,10 +1,31 @@
+import os
 from flask import Flask
+from flask import render_template
+from models import db
+from api_v1 import api as api_v1
 
 app = Flask(__name__)
+app.register_blueprint(api_v1, url_prefix='/api/v1')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 @app.route('/')
 def hello():
     return "Hello World!"
-    
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'adnfkansdkfnadkfnefi'
+
+db.init_app(app) # 설정 초기화
+db.app = app
+db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
